5a035eac4ec6b1387c89941f13de10e28482b9cb
saul <saul.reyna90@gmail.com>
2021-03-07 02:30:09 -0500

    file names to sass

--- backend/go.mod
+++ backend/go.mod
@@ -1,5 +1,5 @@
-module github.com/realtime_chat
+module github.com/ideal-octo-umbrella
 
 go 1.16
 
-require github.com/gorilla/websocket v1.4.2 // direct
+require github.com/gorilla/websocket v1.4.2
--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import { connect, sendMsg } from "./api"
 import Header from './components/Header/Header'
 import ChatHistory from './components/ChatHistory/ChatHistory'
